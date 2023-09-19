44ec17283b85be866977b31086e5a44a59b8f951
amormz <amormz@foxmail.com>
2023-09-19 22:04:28 +0800

    feat: springboot application

---
+++ fortunate-bootstrap/src/main/java/pers/zymir/fortunate/FortunateApplication.java
@@ -0,0 +1,11 @@
+package pers.zymir.fortunate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortunateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FortunateApplication.class);
+    }
+}
